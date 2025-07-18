3956038af8d1489df4aa1dcd8d1dfddab67c6603
sreelekshmi <sreelekshmianithakrishnan@yahoo.com>
2025-07-18 16:54:53 -0400

    project is Ready

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const mainRouter = require("./routes/index");
+
 const { PORT = 3001 } = process.env;
 const app = express();
 
--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,5 @@
-const { getUsers, createUser, getUser } = require("../controllers/users");
 const router = require("express").Router();
+const { getUsers, createUser, getUser } = require("../controllers/users");
 
 router.get("/", getUsers);
 router.get("/:userId", getUser);
