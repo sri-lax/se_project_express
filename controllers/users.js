470ad73bc68d9b89f441e3aef0f533fc7bc3fd50
sreelekshmi <sreelekshmianithakrishnan@yahoo.com>
2025-07-21 19:43:06 -0400

    Updated Project 3

--- controllers/users.js
+++ controllers/users.js
@@ -1,148 +1,42 @@
-const ClothingItem = require("../models/clothingItem");
+const User = require("../models/user");
 const { STATUS_CODES } = require("../utils/constants");
 
-const createItem = (req, res) => {
-  console.log("Request body:", req.body);
-
-  const { name, weather, imageUrl } = req.body;
-
-  ClothingItem.create({ name, weather, imageUrl, owner: req.user._id })
-    .then((item) => res.status(STATUS_CODES.CREATED).send({ data: item }))
+const getUsers = (req, res) => {
+  User.find({})
+    .then((users) => res.status(STATUS_CODES.OK).send(users))
     .catch((err) => {
-      console.error("Create Error:", err);
-      if (err.name === "ValidationError") {
-        return res
-          .status(STATUS_CODES.BAD_REQUEST)
-          .send({ message: "Invalid item data", error: err.message });
-      }
-      return res
-        .status(STATUS_CODES.DEFAULT)
-        .send({ message: "Error from createItem", error: err.message });
+      console.error(err);
+      return res.status(STATUS_CODES.DEFAULT).send({ message: err.message });
     });
 };
 
-const getItems = (req, res) => {
-  ClothingItem.find({})
-    .then((items) => res.status(STATUS_CODES.OK).send(items))
+const createUser = (req, res) => {
+  const { name, avatar } = req.body;
+
+  User.create({ name, avatar })
+    .then((user) => res.status(STATUS_CODES.CREATED).send(user))
     .catch((err) => {
-      console.error("Get Error:", err);
-      res.status(500).send({ message: "GetItem Failed", error: err.message });
+      console.error("Create User Error:", err);
+      next(err);
     });
 };
 
-const updateItem = (req, res) => {
-  const { itemId } = req.params;
-  const { imageUrl } = req.body;
-
-  console.log("Update:", itemId, imageUrl);
-
-  ClothingItem.findByIdAndUpdate(itemId, { $set: { imageUrl } }, { new: true })
-    .orFail(() => new Error("Item not found"))
-    .then((item) => res.status(STATUS_CODES.OK).send({ data: item }))
+const getUser = (req, res) => {
+  const { userId } = req.params;
+  User.findById(userId)
+    .orFail()
+    .then((user) => res.status(STATUS_CODES.OK).send(user))
     .catch((err) => {
-      console.error("Update Error:", err);
-      if (err.name === "ValidationError") {
-        return res
-          .status(STATUS_CODES.BAD_REQUEST)
-          .send({ message: "Invalid update data", error: err.message });
-      }
-      return res
-        .status(STATUS_CODES.DEFAULT)
-        .send({ message: "Error from updateItem", error: err.message });
-    });
-};
-
-const deleteItem = (req, res) => {
-  const { itemId } = req.params;
-
-  ClothingItem.findByIdAndDelete(itemId)
-    .orFail(() => new Error("Item not found"))
-    .then(() => res.sendStatus(STATUS_CODES.NO_CONTENT))
-    .catch((err) => {
-      console.error("Delete Error:", err);
-      if (err.name === "CastError") {
-        return res
-          .status(STATUS_CODES.BAD_REQUEST)
-          .send({ message: "Invalid item ID format", error: err.message });
-      }
-      if (err.message === "Item not found") {
+      console.error(err);
+      if (err.name === "DocumentNotFoundError") {
         return res
           .status(STATUS_CODES.NOT_FOUND)
-          .send({ message: "Item not found", error: err.message });
+          .send({ message: "User not found" });
       }
       return res
-        .status(STATUS_CODES.DEFAULT)
-        .send({ message: "Error from deleteItem", error: err.message });
+        .status(STATUS_CODES.BAD_REQUEST)
+        .send({ message: "Invalid user ID format" });
     });
 };
 
-const likeItem = (req, res) => {
-  const { itemId } = req.params;
-
-  ClothingItem.findByIdAndUpdate(
-    itemId,
-    { $addToSet: { likes: req.user._id } },
-    { new: true }
-  )
-    .orFail(() => new Error("Item not found"))
-    .then((item) => res.status(STATUS_CODES.OK).send({ data: item }))
-    .catch((err) => {
-      console.error("Like Error:", err);
-
-      if (err.name === "CastError") {
-        return res
-          .status(STATUS_CODES.BAD_REQUEST)
-          .send({ message: "Invalid item ID format", error: err.message });
-      }
-
-      if (err.message === "Item not found") {
-        return res
-          .status(STATUS_CODES.NOT_FOUND)
-          .send({ message: "Item not found", error: err.message });
-      }
-
-      return res
-        .status(STATUS_CODES.DEFAULT)
-        .send({ message: "Error from likeItem", error: err.message });
-    });
-};
-
-const unlikeItem = (req, res) => {
-  const { itemId } = req.params;
-
-  ClothingItem.findByIdAndUpdate(
-    itemId,
-    { $pull: { likes: req.user._id } },
-    { new: true }
-  )
-    .orFail(() => new Error("Item not found"))
-    .then((item) => res.status(STATUS_CODES.OK).send({ data: item }))
-    .catch((err) => {
-      console.error("Unlike Error:", err);
-
-      if (err.name === "CastError") {
-        return res
-          .status(STATUS_CODES.BAD_REQUEST)
-          .send({ message: "Invalid item ID format", error: err.message });
-      }
-
-      if (err.message === "Item not found") {
-        return res
-          .status(STATUS_CODES.NOT_FOUND)
-          .send({ message: "Item not found", error: err.message });
-      }
-
-      return res
-        .status(STATUS_CODES.DEFAULT)
-        .send({ message: "Error from unlikeItem", error: err.message });
-    });
-};
-
-module.exports = {
-  createItem,
-  getItems,
-  updateItem,
-  deleteItem,
-  likeItem,
-  unlikeItem,
-};
+module.exports = { getUsers, createUser, getUser };
